ae6191f4a45bbff4b72d90441f5f84f0363c1dd0
Colton <kaiserc2@seattleu.edu>
2020-11-02 17:16:43 -0800

    Refactor decrement count in app.js.

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -3,26 +3,32 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-    return (
-      <div data-test="app-component">
-        <h1 data-test="counter-display">
-          The counter is currently <span data-test="count">{count}</span>
-        </h1>
-        <button
-          data-test="increment-button"
-          onClick={() => setCount(count + 1)}
-        >
-          Increment counter
-        </button>
-        <button
+  const [count, setCount] = useState(0);
+  const [error, setError] = useState(false);
+
+  const decrementCount = () => {
+    return count < 0 ? setError(true) && setCount(0) : setCount(count - 1);
+  };
+
+  return (
+    <div data-test="app-component">
+      <h1 data-test="counter-display">
+        The counter is currently <span data-test="count">{count}</span>
+      </h1>
+      <button data-test="increment-button" onClick={() => setCount(count + 1)}>
+        Increment counter
+      </button>
+      <button
         data-test="decrement-button"
-        onClick={() => setCount(count - 1)}
-        >
-          Decrement counter
-        </button>
-      </div>
-    );
+        onClick={() =>
+          decrementCount()
+        }
+      >
+        Decrement counter
+      </button>
+      <div data-test="error-message" className={error ? null : "hidden"}></div>
+    </div>
+  );
 }
 
 export default App;
