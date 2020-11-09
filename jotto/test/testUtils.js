7e8e59dd724b0d01cf46ae1dd3f86d3351883591
Colton <kaiserc2@seattleu.edu>
2020-11-09 13:37:03 -0800

    Fill out render, success false, and success true tests.

--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,11 +1,9 @@
-import { ShallowWrapper } from "enzyme"
-
 /**
  * Return node(s) with the given data-test attribute.
- * @param {ShallowWrapper} wrapper 
- * @param {string} val 
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {string} val - Value of data-test attribute for search.
  * @returns {ShallowWrapper}
  */
-export const findyByTestAttr = (wrapper, val) => {
-    return wrapper.find(`[data-test=${val}]`)
-}
+export const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test="${val}"]`);
+  }
