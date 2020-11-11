d98bccd8537406bc26ecdc17044ed5df630ee6cb
Colton <kaiserc2@seattleu.edu>
2020-11-11 13:08:17 -0800

    Fill in conditionals for GuessedWords component.

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -2,8 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const GuessedWords = (props) => {
+  let contents;
+  if (!props.guessedWords.length) {
+    contents = (
+      <span data-test="guess-instructions">Try to guess the secret word!</span>
+    );
+  }
   return (
-  <div />
+  <div data-test="component-guessed-words">{contents}</div>
   )
 };
 
