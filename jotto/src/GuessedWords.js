ce95b690b160ac9142d9c17d6b935e9a5364bfc3
Colton <kaiserc2@seattleu.edu>
2020-11-11 12:50:54 -0800

    Set up prop types in GuessedWords component.

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 const GuessedWords = (props) => {
-    return (
-        <div />
-    )
-}
+  return (
+  <div />
+  )
+};
 
-GuessedWords.PropTypes = {
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
-}
-
-export default GuessedWords
+export default GuessedWords;
