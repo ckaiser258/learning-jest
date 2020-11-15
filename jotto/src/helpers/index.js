7e9596e021bce800ec40144556f24c217e55e9d4
Colton <kaiserc2@seattleu.edu>
2020-11-15 13:28:51 -0800

    Create index.js file for letter match count function.

---
+++ jotto/src/helpers/index.js
@@ -0,0 +1,9 @@
+/**
+ * @method getLetterMatchCount - getLetterMatchCount
+ * @param {string} guessedWord  - Guessed word.
+ * @param {string} secretWord - Secret word.
+ * @returns {number} - Number pf letters matched between guessed word and secret word.
+ */
+export function getLetterMatchCount(guessedWord, secretWord) {
+    
+}
