6ee42352c05d7f37245d814a6b3fc9475da2f619
Colton <kaiserc2@seattleu.edu>
2020-11-27 15:00:58 -0800

    Add createStoreWithMiddleware functionality.

--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,17 +1,19 @@
-import checkPropTypes from "check-prop-types";
-import {createStore} from 'redux'
+import checkPropTypes from 'check-prop-types';
+import { createStore, applyMiddleware } from 'redux';
 
-import rootReducer from '../src/reducers'
+import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 /**
  * Create a testing store with imported reducers, middleware, and initial state.
- *  globals: rootReducer
- * @param {object} initialState  - Initial state for store.
+ *  globals: rootReducer, middlewares.
+ * @param {object} initialState - Initial state for store.
  * @function storeFactory
  * @returns {Store} - Redux store.
  */
-export const storeFactory = initialState => {
-  return createStore(rootReducer, initialState)
+export const storeFactory = (initialState) => {
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
 
 /**
@@ -22,14 +24,13 @@ export const storeFactory = initialState => {
  */
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
-};
+}
 
 export const checkProps = (component, conformingProps) => {
   const propError = checkPropTypes(
     component.propTypes,
     conformingProps,
-    "prop",
-    component.name
-  );
+    'prop',
+    component.name);
   expect(propError).toBeUndefined();
-};
+}
